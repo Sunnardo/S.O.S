46ff84d4549fed7a5c0031d195b71bb779d02acd
Suna <pau.fernandezo@educem.net>
2024-10-09 16:41:51 +0200

    UPDATE AL README ++
    Update al readme i còpia de la carpeta del CosmosKernel.

---
+++ CosmosKernelSuna/CosmosKernelSuna/Kernel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace CosmosKernelSuna
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Welcome to Suna's Cosmos S.O. presented by Pau Fernandez Osuna");
+            Console.WriteLine("Benvingut al S.O. que estem desenvolupant en M6, chaval!");
+            Console.WriteLine("Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Estàs preparat per mals de cap desenvolupant el kernel d'aquest S.O.? Ganes? En
 L'entorn per poder treballar en les marvelles de desenvolupar sistemes operatius és el Microsoft Visual Studio.
 Amés, necessitarem la versió 17.6 de VMware si no volem tenir problemes i algun mal de cap.. (els tindràs igual).
 Entre d'altres dependències com el .NET framework 6..
+Si has escalat el monte del destino i has pogut arribar fins aquí, felicitats. El Kernel que està en desenvolupament
+es troba a la carpeta "CosmosKernelSuna", disfruta.
 
 En un futur s'anirà implementant petites millores que fagin d'aquest S.O. un producte millor i més complet. 
 Ara mateix no entenc molt de la materia.
